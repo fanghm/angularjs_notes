45afacead2270bb17b3e7ca21a585e5daa0ac551
Frank <14533348@qq.com>
2017-07-04 16:57:06 +0800

    1.3 Index HTML Setup

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
  * Created by Frank on 2017/7/4.
  */
 (function () {
-    var app = angular.module('store', []);
+    var app = angular.module('gem_store', []);
     app.controller('StoreController', function () {
-        alert('Hello AngularJS!');
+        // alert('Hello AngularJS!');
+        this.product = gem;
     });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Priceless gem!'
+    }
 })();
