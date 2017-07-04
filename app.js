17325382e74e4f6521c1d5af6192299bf283b502
Frank <14533348@qq.com>
2017-07-04 14:55:01 +0800

    1.1 ramp up

---
+++ app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Frank on 2017/7/4.
+ */
+(function () {
+    var app = angular.module('store', []);
+    app.controller('StoreController', function () {
+        alert('Hello AngularJS!');
+    });
+})();
