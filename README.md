203522a689e93720b85efe7d7797266633035d96
Frank <14533348@qq.com>
2017-07-05 10:27:54 +0800

    1.5 Built-in Directives

---
+++ README.md
@@ -0,0 +1,19 @@
+#AngularJS study notes
+=====
+
+##methods
+```javascript
+    var app = angular.module('test', []);
+    app.controller('TestController', function () {
+        // ...
+    });
+```
+
+##directives
+* ng-app
+* ng-controller
+
+* ng-show
+* ng-hide
+
+* ng-repeat
--- app.js
+++ app.js
@@ -5,12 +5,23 @@
     var app = angular.module('gem_store', []);
     app.controller('StoreController', function () {
         // alert('Hello AngularJS!');
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: 'Priceless gem!'
-    }
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'Priceless gem!',
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: 'Super gem',
+            price: 29.99,
+            description: 'Gorgeous gem!',
+            canPurchase: false,
+            soldOut: false
+        },
+    ];
 })();
